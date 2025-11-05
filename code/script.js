6ed2faa005a59d3521a8f4d4e08e73d8166bca70
João César Vaz <joaocesarvazocm@gmail.com>
2025-11-05 16:28:30 -0300

    Create script.js

---
+++ code/script.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.querySelector(".dark-mode-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
+});
